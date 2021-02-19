b8baca145716d23bb6193b7a3cb236fee298c18a
coolphynix <sandeepsen09@gmail.com>
2021-02-19 15:47:13 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
+const particlesOption = {
+		    "particles": 
+		    {
+		           "number": 
+		           {
+			            "value": 80,
+			            "density": 
+			            {
+			                "enable": true,
+			                "value_area": 800
+			            }
+			        }
+    			}
+    		}
+
 class App extends Component {
+	constructor() {
+		super();
+		this.state ={
+			input:'',
+		}
+
+	}
+
+	onInputChange = (event) =>{
+		console.log(event.target.value);
+	}
+
+
   render() {
   	 return (
-    <div className="App">
-     <Navigation />
-     {/*<Logo/>
-     <ImageLinkForm/>
-     <FaceRecognition/>*/}
-    </div>
-  );
-
-  }
+		    <div className="App">
+		     <Particles className='particles' params={particlesOption} />
+		     <Navigation />
+		     <Logo />
+		     <Rank />
+		     <ImageLinkForm onInputChange = {this.onInputChange}/>
+		     {/*<FaceRecognition/>
+		     <UserInformation/>*/
+		 	}
+		    </div>
+  		);
+    }
  }
 
 export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,6 +5,7 @@ return (
 		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 			<p className ='f3 link dim black underline pa3 pointer'> Sign Out </p>
 		</nav>
+		
 	);
 }
 
